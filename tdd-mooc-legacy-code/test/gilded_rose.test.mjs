06dc0c066df411013648a5ec2af870bb2c5c7862
SanttuF <santtu.f@gmail.com>
2024-03-21 06:04:34 +0200

    added tests

--- tdd-mooc-legacy-code/test/gilded_rose.test.mjs
+++ tdd-mooc-legacy-code/test/gilded_rose.test.mjs
@@ -1,5 +1,5 @@
 import { describe, test } from "vitest";
-import { expect, should } from "chai";
+import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
@@ -12,7 +12,6 @@ describe("Gilded Rose", () => {
   test("foo", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
     expect(items[0].quality).to.equal(0);
     expect(items[0].sellIn).to.equal(-1);
   });
@@ -21,33 +20,65 @@ describe("Gilded Rose", () => {
     const gildedRose = new Shop([new Item("foo", 100, 110), new Item("foo", 0, 5)])
     const items = gildedRose.updateQuality()
     expect(items[0].quality).to.equal(109);
+    expect(items[0].sellIn).to.equal(99);
+    expect(items[1].quality).to.equal(3);
     expect(items[1].sellIn).to.equal(-1);
   })
   
   test("Backstage passes to a TAFKAL80ETC concert", () => {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10), new Item("Backstage passes to a TAFKAL80ETC concert", 8, 10), new Item("Backstage passes to a TAFKAL80ETC concert", 1000, 100)]);
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 11, 50), 
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 51), 
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 51),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 100, 100),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 1),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 50, 6),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 49, 5),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[1].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[2].name).to.equal("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[0].quality).to.equal(13);
-    expect(items[1].quality).to.equal(12);
-    expect(items[2].quality).to.equal(100);
+    expect(items[0].quality).to.equal(50);
+    expect(items[1].quality).to.equal(51);
+    expect(items[2].quality).to.equal(51);
+    expect(items[3].quality).to.equal(100);
+    expect(items[4].quality).to.equal(4);
+    expect(items[5].quality).to.equal(7);
+    expect(items[6].quality).to.equal(6);
+    expect(items[7].quality).to.equal(50);
+    expect(items[8].quality).to.equal(50);
+
+    expect(items[0].sellIn).to.equal(10);
+    expect(items[1].sellIn).to.equal(9);
+    expect(items[2].sellIn).to.equal(0);
+    expect(items[3].sellIn).to.equal(99);
+    expect(items[4].sellIn).to.equal(0);
+    expect(items[5].sellIn).to.equal(49);
+    expect(items[6].sellIn).to.equal(48);
+    expect(items[7].sellIn).to.equal(9);
+    expect(items[8].sellIn).to.equal(4);
   });
 
   test('Aged Brie', () => {
-    const gildedRose = new Shop([new Item('Aged Brie', 0, 0)])
+    const gildedRose = new Shop([
+      new Item('Aged Brie', 51, 51), 
+      new Item('Aged Brie', 50, 50),
+      new Item('Aged Brie', 1, 1)])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).to.equal('Aged Brie')
-    expect(items[0].sellIn).to.equal(-1)
-    expect(items[0].quality).to.equal(2)
+    expect(items[0].sellIn).to.equal(50)
+    expect(items[0].quality).to.equal(51)
+    expect(items[1].sellIn).to.equal(49)
+    expect(items[1].quality).to.equal(50)
+    expect(items[2].sellIn).to.equal(0)
+    expect(items[2].quality).to.equal(2)
   })
 
-  test('"Sulfuras, Hand of Ragnaros"', () => {
-    const gildedRose = new Shop([new Item('"Sulfuras, Hand of Ragnaros"', 0, 0)])
+  test('Sulfuras, Hand of Ragnaros', () => {
+    const gildedRose = new Shop([new Item('Sulfuras, Hand of Ragnaros', 0, 0),
+      new Item('Sulfuras, Hand of Ragnaros', 5, 5)])
     const items = gildedRose.updateQuality()
-    expect(items[0].name).to.equal('"Sulfuras, Hand of Ragnaros"')
-    expect(items[0].sellIn).to.equal(-1)
+    expect(items[0].sellIn).to.equal(0)
     expect(items[0].quality).to.equal(0)
+    expect(items[1].sellIn).to.equal(5)
+    expect(items[1].quality).to.equal(5)
   })
 });
